e9286126aca8c948d072d226219ef57dad14f639
WittBulter <nanazuimeng123@gmail.com>
2019-05-31 01:31:37 +0800

    feat: auto redirect to home

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const pages = require('./pages')
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.url === '/') {
+    req.url = '/wittbulter/pages'
+  }
+  
   if (!/(\/|=)[a-zA-Z0-9]+$/.test(req.url)) return ''
   const { url, dark } = pages.getQuery(req.url)
   
